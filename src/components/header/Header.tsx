2d9224586001058a0ac35632b3fd3686423c585a
Tuba Hosseini <tubahosseini@gmail.com>
2024-05-15 20:52:17 +0330

    header added

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <h1 className="bg-yellow-200 text-center p-3 rounded-b-[40px] mb-5 font-bold text-lg">
+      Contacts Web Application
+    </h1>
+  );
+}
+
+export default Header;
